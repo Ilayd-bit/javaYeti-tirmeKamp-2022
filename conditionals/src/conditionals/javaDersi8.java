3abb7d8bd87063839853a994396a2f54dc9e2d4d
Ilayd-bit <113545559+Ilayd-bit@users.noreply.github.com>
2022-09-17 21:51:25 +0300

    commit1

---
+++ conditionals/src/conditionals/javaDersi8.java
@@ -0,0 +1,22 @@
+package conditionals;
+
+public class javaDersi8 {
+
+	public static void main(String[] args) {
+		int sayı = 25;
+		if (sayı<20) {
+			System.out.println("Sayı 20 den küçüktür");
+		}
+		
+		
+		else if(sayı== 20) {
+			System.out.println("Sayı 20 ye eşittir");
+			
+		} else {
+			System.out.println("Sayı 20 den büyüktür");
+			//sayıya eşit denk olması için iki eşittir kullanılır
+			
+		}
+	}
+}
+
