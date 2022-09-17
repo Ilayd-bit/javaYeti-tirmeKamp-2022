8cea0e8312514764bd790f831da8031c70d3ecb0
Ilayd-bit <113545559+Ilayd-bit@users.noreply.github.com>
2022-09-17 22:00:30 +0300

    commit1

---
+++ sesliHarfler/src/sesliHarfler/javaDersi20.java
@@ -0,0 +1,24 @@
+package sesliHarfler;
+
+public class javaDersi20 {
+
+	public static void main(String[] args) {
+		char harf = 'E';
+		
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalın sesli harfler");
+			break;
+			default:
+				System.out.println("İnce sesli harfler");
+		
+		
+		
+		}
+
+	}
+
+}
