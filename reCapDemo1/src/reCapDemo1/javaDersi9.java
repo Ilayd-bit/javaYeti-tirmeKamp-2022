19e1467e648ad389cc7455c65a309178885d505f
Ilayd-bit <113545559+Ilayd-bit@users.noreply.github.com>
2022-09-17 21:58:23 +0300

    commit1

---
+++ reCapDemo1/src/reCapDemo1/javaDersi9.java
@@ -0,0 +1,26 @@
+package reCapDemo1;
+
+public class javaDersi9 {
+
+	public static void main(String[] args) {
+		int sayı1 = 26;
+		int sayı2 = 400;
+		int sayı3 = 2;
+		int enBuyuk = sayı1;
+		
+		if(enBuyuk<sayı2) {
+			enBuyuk = sayı2;
+			
+		}
+		
+		if(enBuyuk<sayı3) {
+			enBuyuk = sayı3; } 
+		System.out.println("En büyük=" + enBuyuk);
+		
+	}
+}
+		
+			
+			
+		
+
