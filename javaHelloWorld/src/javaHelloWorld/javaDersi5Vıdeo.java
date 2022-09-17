fb9ea5143b8f639b77a861258aa18666f2e2a515
Ilayd-bit <113545559+Ilayd-bit@users.noreply.github.com>
2022-09-17 21:55:08 +0300

    commit1

---
+++ javaHelloWorld/src/javaHelloWorld/javaDersi5Vıdeo.java
@@ -0,0 +1,17 @@
+package javaHelloWorld;
+
+public class javaDersi5Vıdeo {
+
+	public static void main(String[] args) {
+	System.out.println("Merhaba Java");
+	//Programlarda sıralama yapmak adına kullanılır. (.) Dışarı çıktı vereceksek system out kullanılır.
+	//intellisense programcıya kodunu yazarken yardımcı görevi görür.
+	System.out.println("Merhaba Java 2");
+	
+	
+	
+	
+
+	}
+
+}
