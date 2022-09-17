fbb5dc47e698cd343d72b0142c4996113bbdcec0
Ilayd-bit <113545559+Ilayd-bit@users.noreply.github.com>
2022-09-17 22:02:07 +0300

    commit1

---
+++ variables/src/javaDersiVıdeo6.java
@@ -0,0 +1,25 @@
+
+public class javaDersiVıdeo6 {
+
+	public static void main(String[] args) {
+		// case sensitive büyük küçük harfe hasssastır.
+		//reusebility tekrar kullanılabilirlik demektir
+		//javada tam sayılar integer ile kullanılır.
+		int ogrenciSayısı = 16;
+		String mesaj ="Öğrenci Sayısı:";
+		System.out.println(mesaj+ ogrenciSayısı);
+		System.out.println(mesaj+ ogrenciSayısı);
+		System.out.println(mesaj+ ogrenciSayısı);
+		System.out.println(mesaj+ ogrenciSayısı);
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
