c19fdbafdcd70f8d92f4574ab35ed76f78c39661
Ilayd-bit <113545559+Ilayd-bit@users.noreply.github.com>
2022-09-17 21:52:47 +0300

    commit1

---
+++ dataTypes/src/dataTypes/javaDersiVıdeo7.java
@@ -0,0 +1,35 @@
+package dataTypes;
+
+public class javaDersiVıdeo7 {
+
+	public static void main(String[] args) {
+		// Değişken tanımladığın zaman onu istediğin şekilde tanımlayamazssın
+		double sayı = 15.6;
+		//incompatible types uyumsuz tipler demektir.
+		//tam sayının sınırlarına çıkarsak bunu integer ile yapamayız. Bellekte kapladığı yere kadar sınırı vardır
+		//byte 127 ve -128 kadar değer yazabiliriz.
+		//short 2 byte'lık bir alan kaplar kapladığı yer kadar yazabiliriz
+		//daha büyük sayılar yazacağımız zaman long kullanılır.(8 bytes) Her şeyi long olarak tanımlayabiliriz ancak çok fazla bellek tüketimi yapmış oluruz.
+		//ondalıklı sayıların double ile yazılır
+		//char tek karakter tutan veri tipidir
+		char karakter = 'a';
+		//boolean doğru veya yanlıştır sıfır veya birdir.
+		boolean dogruMu = false;
+		System.out.println("double sayı");
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+	
+				
+
+	}
+
+}
