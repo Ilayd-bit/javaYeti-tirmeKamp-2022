b74223f2de71c9c27305cb455ec86c23275ff007
Ilayd-bit <113545559+Ilayd-bit@users.noreply.github.com>
2022-09-23 23:42:35 +0300

    odev2

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/DortIslem.java
@@ -0,0 +1,23 @@
+package reCapDemo_Classes;
+
+public class DortIslem {
+	public int Topla (int sayı1, int sayı2) {
+		return sayı1+sayı2;
+		
+	}
+	
+	public int Cıkar (int sayı1, int sayı2) {
+		return sayı1-sayı2;
+		
+	}
+	public int Carp (int sayı1, int sayı2) {
+		return sayı1*sayı2;
+		
+	}
+	public int Bol (int sayı1, int sayı2) {
+		return sayı1/sayı2;
+		
+	}
+	
+
+}
